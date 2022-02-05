dcef7270e0987b51679add03b239e0e0f9bae0e6
Alim Koca <kinetixcicocuk@gmail.com>
2022-02-05 15:05:41 +0300

    read function is ready for everyone

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 define build_baml
-	gcc main.c -o baml
+	@gcc main.c -o baml -w
+	@echo "Compiled"
 endef
 
 define test_baml
+	@echo "Testing:"
 	/usr/bin/baml --bash ./example.baml
 endef
 
@@ -13,14 +15,15 @@ endef
 build:
 	$(build_baml)
 	@cp libbaml/baml.h /usr/include/
-	
+	@cp libbaml/read.h /usr/include
 
 install:
 	$(build_baml)
 	install -m 755 ./baml /usr/bin/baml
 	@$(test_baml)
 	@cp libbaml/baml.h /usr/include/
-
+	@cp libbaml/read.h /usr/include/
+	@echo "Installed perfectly!!!"
 uninstall:
 	rm /usr/bin/baml
 
---
+++ libbaml/read.h
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *read_entry(int argc, char *argv[]){
+	FILE	*fp;
+	char	**format;
+	int		first_arg;
+	char	buffer[3065];
+	char	*store;
+	char	hopen;
+	size_t	stcursor;
+	size_t	lfile;
+
+	buffer[3064] = 0;
+
+	if (argc > 1)
+	{
+		if (!(format = valid_arg(argv[1])))
+		{
+			printf("Invalid determinator argument.\n");
+			return (1);
+		}
+		if (!strncmp(argv[1], "--", 2))
+			first_arg = 2;
+		else
+			first_arg = 1;
+		for (int i = first_arg; i < argc; i++)
+		{
+			fp = fopen(argv[i], "r");
+			store = (char *) calloc(1, sizeof(char));
+			hopen = 0;
+			lfile = 0;
+
+			if (fp == NULL)
+			{
+				printf("Cannot open file: %s\n", argv[i]);
+				continue ;
+			}
+			while (fgets(buffer, 3065, fp))
+			{
+				stcursor = dstr(store, buffer);
+				if (store[stcursor - 1] == '\n')
+				{
+					lfile++;
+					if (store[stcursor - 2] == HEADERTOKEN)
+					{
+						pheader(store, format, &hopen);
+					}
+					else if ((stcursor = valid_token(store)))
+					{
+						if(!hopen)
+						{
+							printf("Incomplete entry at line %ld\n", lfile);
+							return (1);
+						}
+						pentry(&store[stcursor], format, &hopen);
+					}
+					free(store);
+					store = calloc(1, sizeof(char));
+				}
+			}
+			if (hopen)
+				return format[3];
+			free(store);
+			fclose(fp);
+		}
+	}
+	else
+		printf("Incomplete arguments.\n");
+	return format[3];
+}
